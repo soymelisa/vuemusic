2898843aaf91d3123892886a7206c5ba38665f84
Melisa Perez <soymelisa@yahoo.com>
2018-02-27 23:54:41 -0600

    otra vez modifiqué las URL porque el problema venía del hash de webpack

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template lang="pug">
 #app
-  img(src='https://soymelisa.github.io/vuemusic/dist/logo.png')
+  img(src='dist/logo.png')
   h1 Vuemusicm
   select(v-model="selectedCountry")
     option(v-for ="country in countries" :value="country.value") {{ country.name }}
