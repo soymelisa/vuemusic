c4f7713181c3664ae8ae4eebbaa18835593b092b
Melisa Perez <soymelisa@yahoo.com>
2018-02-27 23:20:00 -0600

    Agregando el progreso con un CSS Spinner

--- src/App.vue
+++ src/App.vue
@@ -4,6 +4,7 @@
   h1 tzimusicm
   select(v-model="selectedCountry")
     option(v-for ="country in countries" :value="country.value") {{ country.name }}
+  spinner(v-show="loading")
   ul
     artist(v-for="artist in artists"
     v-bind:artist="artist" v-bind:key="artist.mbid")
@@ -11,6 +12,7 @@
 
 <script>
 import Artist from './components/Artist.vue'
+import Spinner from './components/Spinner.vue'
 import getArtists from './api'
 
 export default {
@@ -23,17 +25,22 @@ export default {
         { name: 'Colombia', value: 'colombia' },
         { name: 'España', value: 'spain' },
       ],
-      selectedCountry: 'mexico'
+      selectedCountry: 'mexico',
+      loading: true
     }
   },
   components: {
-    Artist
+    Artist,
+    Spinner
   },
   methods: {
     refreshArtists() {
       const self = this
+      this.loading = true
+      this.artists = []
       getArtists(this.selectedCountry)
       .then(function (artists) {
+        self.loading = false
         self.artists = artists
       })
     }
